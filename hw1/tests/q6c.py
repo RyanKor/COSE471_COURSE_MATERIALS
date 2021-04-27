3f9b39ea63f471073a854610904923a0786dac21
ryanKor <equus3144@gmail.com>
2021-04-27 10:19:47 +0900

    hw3, project1 added

--- hw1/tests/q6c.py
+++
@@ -1,4 +0,0 @@
-test = {   'name': 'q6c',
-    'points': 2,
-    'suites': [{'cases': [{'code': '>>> prob_6c >= 0 and prob_6c <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
-
--- hw1/tests/q7d.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q7d',
-    'points': 2,
-    'suites': [{'cases': [{'code': '>>> trump_wins(1000) in [0, 1]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
